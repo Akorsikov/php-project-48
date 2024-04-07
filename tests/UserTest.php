cfbdb808a4bcc6065736fe052a207308c23f40f3
[Akorsikov] <[akors@mail.ru]>
2024-04-07 16:32:55 +0300

    refactor: relocate expected result into ficture

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -11,10 +11,10 @@ class UserTest extends TestCase
 {
     public function testGenDiff(): void
     {
-        $path1 = './tests/file1.json';
-        $path2 = './tests/file2.json';
-        $differences = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}\n";
+        $path1 = './tests/fixtures/file1.json';
+        $path2 = './tests/fixtures/file2.json';
+        $fileDiff = './tests/fixtures/difference.txt';
 
-        $this->assertEquals($differences, genDiff($path1, $path2));
+        $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
     }
 }
