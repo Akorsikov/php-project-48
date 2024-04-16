b715673ca2286d8138a80c5f7d3c39d344fdf957
[Akorsikov] <[akors@mail.ru]>
2024-04-16 08:55:41 +0300

    feat: add tests for flat files & comment tests for recursive files

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,24 +9,41 @@ use function Php\Project\Diff\genDiff;
 
 class UserTest extends TestCase
 {
-    public function testGenDiff(): void
-    {
-        $path1 = './tests/fixtures/file1.json';
-        $path2 = './tests/fixtures/file2.json';
-        $fileDiff = './tests/fixtures/difference.txt';
+//     public function testGenDiff(): void
+//     {
+//         $path1 = './tests/fixtures/file1.json';
+//         $path2 = './tests/fixtures/file2.json';
+//         $fileDiff = './tests/fixtures/difference.txt';
 
-        $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
-    }
+//         $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+//     }
 
-    public function testGenDiff2(): void
-    {
-        $path1 = './tests/fixtures/file2.json';
-        $path2 = './tests/fixtures/file1.json';
-        $fileDiff = './tests/fixtures/difference2.txt';
+//     public function testGenDiff2(): void
+//     {
+//         $path1 = './tests/fixtures/file2.json';
+//         $path2 = './tests/fixtures/file1.json';
+//         $fileDiff = './tests/fixtures/difference2.txt';
 
-        $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
-    }
+//         $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+//     }
 
+public function testGenDiffFlatFiles(): void
+{
+    $path1 = './tests/fixtures/flat-file1.json';
+    $path2 = './tests/fixtures/flat-file2.json';
+    $fileDiff = './tests/fixtures/difference.txt';
+
+    $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+}
+
+public function testGenDiffFlatFiles2(): void
+{
+    $path1 = './tests/fixtures/flat-file2.json';
+    $path2 = './tests/fixtures/flat-file1.json';
+    $fileDiff = './tests/fixtures/difference2.txt';
+
+    $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+}
     public function testGenDiffYmlFiles(): void
     {
         $path1 = './tests/fixtures/file1.yml';
