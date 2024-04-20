a55aec0d3e7622a7f2ee6422febae4ba40634717
[Akorsikov] <[akors@mail.ru]>
2024-04-20 15:33:15 +0300

    feat: tests for flat & recursive structures

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,49 +6,50 @@ use PHPUnit\Framework\TestCase;
 use Php\Project\Diff;
 
 use function Php\Project\Diff\genDiff;
+use function Php\Project\Diff\getFormat;
 
 class UserTest extends TestCase
 {
-//     public function testGenDiff(): void
-//     {
-//         $path1 = './tests/fixtures/file1.json';
-//         $path2 = './tests/fixtures/file2.json';
-//         $fileDiff = './tests/fixtures/difference.txt';
+    public function testGenDiff(): void
+    {
+        $path1 = './tests/fixtures/file1.json';
+        $path2 = './tests/fixtures/file2.json';
+        $fileDiff = './tests/fixtures/diff-12.txt';
 
-//         $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
-//     }
+        $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+    }
 
-//     public function testGenDiff2(): void
-//     {
-//         $path1 = './tests/fixtures/file2.json';
-//         $path2 = './tests/fixtures/file1.json';
-//         $fileDiff = './tests/fixtures/difference2.txt';
+    public function testGenDiff2(): void
+    {
+        $path1 = './tests/fixtures/flat-file2.json';
+        $path2 = './tests/fixtures/flat-file1.json';
+        $fileDiff = './tests/fixtures/flat-diff-21.txt';
 
-//         $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
-//     }
+        $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
+    }
 
 public function testGenDiffFlatFiles(): void
 {
     $path1 = './tests/fixtures/flat-file1.json';
     $path2 = './tests/fixtures/flat-file2.json';
-    $fileDiff = './tests/fixtures/difference.txt';
+    $fileDiff = './tests/fixtures/flat-diff.txt';
 
     $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
 }
 
-public function testGenDiffFlatFiles2(): void
+public function testGenDiffYalmFiles(): void
 {
-    $path1 = './tests/fixtures/flat-file2.json';
-    $path2 = './tests/fixtures/flat-file1.json';
-    $fileDiff = './tests/fixtures/difference2.txt';
+    $path1 = './tests/fixtures/file1.yaml';
+    $path2 = './tests/fixtures/file2.yaml';
+    $fileDiff = './tests/fixtures/diff-12.txt';
 
     $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
 }
     public function testGenDiffYmlFiles(): void
     {
-        $path1 = './tests/fixtures/file1.yml';
-        $path2 = './tests/fixtures/file2.yml';
-        $fileDiff = './tests/fixtures/difference.txt';
+        $path1 = './tests/fixtures/flat-file1.yml';
+        $path2 = './tests/fixtures/flat-file2.yml';
+        $fileDiff = './tests/fixtures/flat-diff.txt';
 
         $this->assertStringEqualsFile($fileDiff, genDiff($path1, $path2));
     }
