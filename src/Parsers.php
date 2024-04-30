3f24d78d3655e953c5a997c50482102e3f1b5289
[Akorsikov] <[akors@mail.ru]>
2024-04-30 13:30:18 +0300

    fix: add an exception to handle a non-compliant extension

--- src/Parsers.php
+++ src/Parsers.php
@@ -17,8 +17,10 @@ function parser(string $fileContent, string $extention): object
     if ($extention === 'yml' or $extention === 'yaml') {
         $object = Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
         $parceContent = json_encode($object);
-    } else {
+    } elseif ($extention === 'json') {
         $parceContent = $fileContent;
+    } else {
+        throw new \Exception("Error: Invalid file extension '{$extention}', use json- or yaml/yml- files !\n");
     }
 
     return json_decode((string) $parceContent, false);
