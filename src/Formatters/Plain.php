11b2cc4dcf9935a181960dfdff21210455551b16
[Akorsikov] <[akors@mail.ru]>
2024-05-09 10:44:54 +0300

    refactor: replace loop "for()" by "array_reduce()"

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -23,51 +23,124 @@ namespace Differ\Formaters\Plain;
  */
 function plain(array $nodes, string $path = ''): string
 {
-    $result = '';
-    $lengthNodes = count($nodes);
-    for ($i = 0; $i < $lengthNodes; $i++) {
+    $result = $path;
+    $result = array_reduce($nodes, function ($carry, $item) use ($path, $nodes) {
+        $newResult = $carry;
         $nameNode = $path;
-        if (array_key_exists('type', $nodes[$i]) and $nodes[$i]['type'] !== 'uncanged') {
-            $nameNode .= "{$nodes[$i]['name']}.";
+        if (array_key_exists('type', $item) or $item['type'] !== 'uncanged') {
+            $nameNode .= "{$item['name']}.";
+            $typeCurNode = $item['type'];
+            $prevNode = getPrevNode($item, $nodes);
+            $nextNode = getNextNode($item, $nodes);
+            // var_dump('NextNode: ', $nextNode);
+            $typePrevNode = (is_null($prevNode)) ? null : $prevNode['type'];
+            $typeNextNode = (is_null($nextNode)) ? null : $nextNode['type'];
+            $namePrevNode = (is_null($prevNode)) ? null : $prevNode['name'];
+            $nameNextNode = (is_null($nextNode)) ? null : $nextNode['name'];
 
-            $typeCurNode = $nodes[$i]['type'];
-            $typePrevNode = ($i > 0) ? $nodes[$i - 1]['type'] : null;
-            $typeNextNode = ($i < ($lengthNodes - 1)) ? $nodes[$i + 1]['type'] : null;
-
-            if (array_key_exists('children', $nodes[$i])) {
-                $result .= plain($nodes[$i]['children'], $nameNode);
+            if (array_key_exists('children', $item)) {
+                $newResult .= plain($item['children'], $nameNode);
             } else {
                 $nameNode = rtrim($nameNode, '.');
-                $value = getNormalisedValue($nodes[$i]['value']);
+                $value = getNormalisedValue($item);
                 if ($typeCurNode === 'deleted') {
-                    if ($typeNextNode === 'added' and $nodes[$i]['name'] === $nodes[$i + 1]['name']) {
-                        $newValue = getNormalisedValue($nodes[$i + 1]['value']);
-                        $result .= "Property '{$nameNode}' was updated. From {$value} to {$newValue}\n";
+                    if ($typeNextNode === 'added' and $item['name'] === $nameNextNode) {
+                        // var_dump('NextNode: ', $nextNode);
+                        $newValue = getNormalisedValue($nextNode);
+                        // $newValue = getNormalisedValue($nextNode['value']);
+                        $newResult .= "Property '{$nameNode}' was updated. From {$value} to {$newValue}\n";
                     } else {
-                        $result .= "Property '{$nameNode}' was removed\n";
+                        $newResult .= "Property '{$nameNode}' was removed\n";
                     }
                 } elseif ($typeCurNode === 'added') {
-                    if ($i === 0 or $typePrevNode !== 'deleted' or $nodes[$i]['name'] !== $nodes[$i - 1]['name']) {
-                        $result .= "Property '{$nameNode}' was added with value: {$value}\n";
+                    if ($typePrevNode !== 'deleted' or $item['name'] !== $namePrevNode) {
+                        $newResult .= "Property '{$nameNode}' was added with value: {$value}\n";
                     }
                 }
             }
         }
-    }
+        return $newResult;
+    }, '');
     return $result;
 }
+/**
+ * Function returns an element (node) of an array (tree)
+ * preceding the given element in this array.
+ *
+ * @param array<mixed> $itemNodes Element (node)
+ * relative to which the preceding element is searched for.
+ * @param array<mixed> $nodes The array in which the search
+ * is performed.
+ *
+ * @return array<mixed>|null The element (node) of the array (tree)
+ * preceding the given element. Null if the given element is the first.
+ */
+function getPrevNode(array $itemNodes, array $nodes): array|null
+{
+
+    $result = array_reduce($nodes, function ($carry, $item) use ($itemNodes) {
+        static $search = true;
+        if ($search) {
+            if ($item === $itemNodes) {
+                $search = false;
+                return $carry;
+            } else {
+                $carry = $item;
+            }
+        }
+        return $carry;
+    });
+    return $result;
+}
+
+/**
+ * The function returns an element (node) of an array (tree) following
+ * the specified element in this array.
+ *
+ * @param array<mixed> $itemNodes The element relative to which
+ * the next element is searched.
+ * @param array<mixed> $nodes The array in which the search is performed.
+ *
+ * @return array<mixed>|null The element (node) of the array (tree)
+ * following the specified element. Null if the given element is the last one.
+ */
+function getNextNode(array $itemNodes, array $nodes): array|null
+{
+    $result = array_reduce($nodes, function ($carry, $item) use ($itemNodes) {
+        static $search = true;
+        static $next = false;
+        if ($search) {
+            if ($item === $itemNodes) {
+                $next = true;
+            } elseif ($next) {
+                $carry = $item;
+            }
+        }
+        if (!is_null($carry)) {
+            $search = false;
+            return $carry;
+        }
+    });
+    return $result;
+}
+
 /**
  * Function returns [complex value] instead of the argument $value,
  * if the argument is an array or adds quotes to the argument
  * if the argument is not a number or one of the following values:
  * 'true', 'false', 'null'. Otherwise returns the argument as is.
  *
- * @param mixed $value
+ * @param array<mixed>|null $node
  *
  * @return float|int|string
  */
-function getNormalisedValue(mixed $value): float|int|string
+function getNormalisedValue(array|null $node): float|int|string
 {
+    if (is_array($node) and array_key_exists('value', $node)) {
+        $value = $node['value'];
+    } else {
+        $value = null;
+    }
     if (is_array($value)) {
         return '[complex value]';
     } elseif (!in_array($value, ['true', 'false', 'null'], true) and !is_numeric($value)) {
