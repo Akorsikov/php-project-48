01721fb05dfa2fadffe70665385bd04cc334ebf0
[Akorsikov] <[akors@mail.ru]>
2024-05-18 16:36:36 +0300

    refactor: use "switch"-expression instead of "if"

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,20 +8,20 @@ use Symfony\Component\Yaml\Yaml;
  * Function converts json or yaml/yml file contents into an object
  *
  * @param string $fileContent content of file
- * @param string $extention extention of file
+ * @param string $extension extention of file
  *
  * @return object object with recursive structure
  */
-function parser(string $fileContent, string $extention): object
+function parser(string $fileContent, string $extension): object
 {
-    if ($extention === 'yml' or $extention === 'yaml') {
-        $object = Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
-        $parceContent = json_encode($object);
-    } elseif ($extention === 'json') {
-        $parceContent = $fileContent;
-    } else {
-        throw new \Exception("Error: Invalid file extension '{$extention}', use json- or yaml/yml- files !\n");
+    switch ($extension) {
+        case 'json':
+            return json_decode((string) $fileContent, false);
+        case 'yaml':
+            return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
+        default:
+            throw new \Exception(
+                "Error: Invalid file extension '{$extension}', use json- or yaml/yml- files !\n"
+            );
     }
-
-    return json_decode((string) $parceContent, false);
 }
