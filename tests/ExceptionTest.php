e1bda68a87a8b70f06372da4efa1012613c3d6d6
[Akorsikov] <[akors@mail.ru]>
2024-05-31 13:48:47 +0300

    refactor: add magic const __DIR__ & exchange file1.txt by stylishDiff.txt for ExceptionTest

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -10,8 +10,8 @@ class ExceptionTest extends TestCase
 {
     public function testException(): void
     {
-        $path1 = './tests/fixtures/file1.txt';
-        $path2 = './tests/fixtures/file2.json';
+        $path1 = __DIR__ . '/fixtures/stylishDiff.txt';
+        $path2 = __DIR__ . '/fixtures/file2.json';
         $message = "Error: Invalid file extension, use json- or yaml/yml- files!\n";
 
         $this->expectException(\Exception::class);
@@ -22,8 +22,8 @@ class ExceptionTest extends TestCase
 
     public function testException2(): void
     {
-        $path1 = './tests/fixtures/file1.yml';
-        $path2 = './tests/fixtures/file2.txt';
+        $path1 = __DIR__ . '/fixtures/file1.yml';
+        $path2 = __DIR__ . '/fixtures/file2.txt';
         $message = "Error: The file '{$path2}' do not exist or are unreadable!\n";
 
         $this->expectException(\Exception::class);
